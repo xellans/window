aca37a0b11dc9c9e59e8d1e9536f22d260dad2c1
xellans <130226258+xellans@users.noreply.github.com>
2024-01-17 21:51:07 +0700

    WindowsStyle_Sample

---
+++ WinStyles/WindowStyle.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WinStyles
+{
+    public partial class WindowStyle : ResourceDictionary
+    {
+
+        static WindowStyle()
+        {
+            CommandManager.RegisterClassCommandBinding(typeof(Window), new CommandBinding(MinimizeCommand, MinimizeExecuted));
+            CommandManager.RegisterClassCommandBinding(typeof(Window), new CommandBinding(MaximizeRestoreCommand, MaximizeRestoreExecuted));
+            CommandManager.RegisterClassCommandBinding(typeof(Window), new CommandBinding(CloseCommand, CloseExecuted));
+
+        }
+        public static readonly RoutedCommand MinimizeCommand = new("Minimize", typeof(WindowStyle));
+
+        public static readonly RoutedCommand MaximizeRestoreCommand = new("MaximizeRestore", typeof(WindowStyle));
+
+        public static readonly RoutedCommand CloseCommand = new("Close", typeof(WindowStyle));
+
+        private static void MinimizeExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var window = (Window)sender;
+            window.WindowState = WindowState.Minimized;
+        }
+        private static void MaximizeRestoreExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var window = (Window)sender;
+            if (window.WindowState == WindowState.Normal)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+        private static void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var window = (Window)sender;
+            window.Close();
+        }
+    }
+}
---
+++ WinStyles/WindowStyleDP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace WinStyles
+{
+    public class WindowStyleDP
+    {
+        #region BackgroundCaption цвет фона заголовка окна
+        public static readonly DependencyProperty BackgroundCaptionProperty =
+            DependencyProperty.RegisterAttached(
+                nameof(GetBackgroundCaption)[3..],
+                typeof(SolidColorBrush),
+                typeof(WindowStyleDP),
+                new PropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#293955"))));
+
+        public static SolidColorBrush GetBackgroundCaption(UIElement element) => (SolidColorBrush)element.GetValue(BackgroundCaptionProperty);
+
+        public static void SetBackgroundCaption(UIElement element, SolidColorBrush value) => element.SetValue(BackgroundCaptionProperty, value);
+        #endregion
+    }
+}
